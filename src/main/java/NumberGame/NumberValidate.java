b3efada95336e29a9e0c06bfbb0aeff2f6ce097b
Hezi HE <hezi.he@thoughtworks.com>
2022-04-21 22:19:01 +0800

    another way to realise return xAyB when input and actual is different

--- src/main/java/NumberGame/NumberValidate.java
+++ src/main/java/NumberGame/NumberValidate.java
@@ -1,28 +1,26 @@
 package NumberGame;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class NumberValidate {
     public String validateDigit(String actual, String input) {
-        //        HashSet<Integer> objects = new HashSet<>();
-        //        actual.chars().forEach(objects::add);
-        //        HashSet<Integer> objects1 = new HashSet<>();
-        //        input.chars().forEach(objects1::add);
-        //        long count = objects.stream().filter(objects1::contains).count();
-        //        long countA = 0;
-        //        for (int i = 0; i < 4; i++) {
-        //            if(actual.charAt(i) == input.charAt(i)){
-        //                countA++;
-        //            }
-        //        }
-        int countA = 0, countB = 0;
-        for (int i = 0; i < 4; i++) {
-            if (input.contains(actual.charAt(i) + "")) {
-                if (actual.charAt(i) == input.charAt(i)) {
-                    countA++;
-                } else {
-                    countB++;
-                }
-            }
-        }
-        return countA + "A" + countB + "B";
+        //without using for
+        long count = actual.chars().filter(m -> input.contains((char)m+"")).count();
+        AtomicInteger i = new AtomicInteger();
+        long countA = actual.chars().filter(m -> (char)m == input.charAt(i.getAndIncrement())).count();
+        return countA+"A"+(count-countA)+"B";
+//        [using for]
+//        int countA = 0, countB = 0;
+//        for (int i = 0; i < 4; i++) {
+//            if (input.contains(actual.charAt(i) + "")) {
+//                if (actual.charAt(i) == input.charAt(i)) {
+//                    countA++;
+//                } else {
+//                    countB++;
+//                }
+//            }
+//        }
+//        return countA + "A" + countB + "B";
+
     }
 }
--- src/test/java/NumberValidateTest.java
+++ src/test/java/NumberValidateTest.java
@@ -25,4 +25,11 @@ class NumberValidateTest {
         assertEquals("1A0B", NumberValidate.validateDigit("1234", "1678"));
     }
 
+    @Test
+    void should_return_xAyB_when_input_is_different_from_actual() {
+        NumberValidate NumberValidate = new NumberValidate();
+
+        assertEquals("2A2B", NumberValidate.validateDigit("1234", "1324"));
+    }
+
 }
